4ac9233cb9c1a8064c6f788877e6acaa0e85c413
PinedaVictor <pinedavictor095@gmail.com>
2020-08-05 23:15:57 -0700

    Added firebase functions js code

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,5 @@
-import * as functions from "firebase-functions";
-import * as sgMail from "@sendgrid/mail";
+// import * as functions from "firebase-functions";
+// import * as sgMail from "@sendgrid/mail";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -9,5 +9,29 @@ import * as sgMail from "@sendgrid/mail";
 //   response.send("Hello from Firebase!");
 // });
 
-export const sendResume = functions.https.onCall((data, context) => {});
-export const contact = functions.https.onCall((data, context) => {});
+// export const sendResume = functions.https.onCall((data, context) => {});
+// export const contact = functions.https.onCall((data, context) => {});
+
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+exports.addMessage = functions.https.onRequest(async (req: any, res: any) => {
+  const original = req.query.text;
+  const writeResult = await admin
+    .firestore()
+    .collection("messages")
+    .add({ original: original });
+
+  res.json({ result: `Message with ID: ${writeResult.id} added.` });
+});
+
+exports.makeUppercase = functions.firestore
+  .document("/messages/{documentId}")
+  .onCreate((snap: any, context: any) => {
+    const original = snap.data().original;
+
+    functions.logger.log("Uppercasing", context.params.documentId, original);
+    const uppercase = original.toUpperCase();
+    return snap.ref.set({ uppercase }, { merge: true });
+  });
