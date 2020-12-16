d14842656e5dad9ae9238cd5b022e138d2751ce7
PinedaVictor <pinedavictor095@gmail.com>
2020-12-16 15:38:25 -0800

    Installed react-spring and added SVGs folder

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import "../Styles/main.scss";
-import { ReactComponent as IDK } from "../idk.svg";
+import { ReactComponent as IDK } from "../SVGs/idk.svg";
 import { Container, Col } from "react-bootstrap";
 
 export const Header: React.FC = () => {
   return (
     <Container className="HeroWrapper">
-      <Col style={{ backgroundColor: "", width: "50%" }}>
-        <IDK />
-        {/* idk */}
+      <Col style={{ backgroundColor: "", width: "100px" }}>
+        <IDK className="initials" />
       </Col>
       <Col style={{ backgroundColor: "green" }}>Vp</Col>
       <Col style={{ backgroundColor: "blue" }}>Vp</Col>
