c4c3cedf28494e5c6b480edebe0c61cccc251619
unknown <pinedavictor095@gmail.com>
2020-12-20 19:04:14 -0800

    Added card structure and placeholder cards

--- src/Components/sections/Projects.tsx
+++ src/Components/sections/Projects.tsx
@@ -1,10 +1,27 @@
 import React from "react";
+import "../../Styles/main.scss";
 import { Container } from "react-bootstrap";
+import { ProjectCard } from "../ui/Card";
 
 export const Projects: React.FC = () => {
+  const projects = [1, 2, 3, 4, 5, 6];
   return (
-    <Container fluid style={{ backgroundColor: "red" }}>
-      <p>Projects</p>
+    <Container fluid style={{ backgroundColor: "", maxWidth: "80rem" }}>
+      <div
+        className="wrapper"
+        style={{
+          backgroundColor: "",
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          margin: "auto",
+          maxWidth: "80rem",
+        }}
+      >
+        {projects.map((item, index) => (
+          <ProjectCard key={index} />
+        ))}
+      </div>
     </Container>
   );
 };
--- src/Components/ui/Card.tsx
+++ src/Components/ui/Card.tsx
@@ -1,20 +1,20 @@
 import React from "react";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Accordion } from "react-bootstrap";
 
 export const ProjectCard: React.FC = () => {
   return (
-    <>
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
+    <div style={{ backgroundColor: "", padding: "5px", margin: "auto" }}>
+      <Card style={{ maxWidth: "25rem", height: "25rem" }}>
+        <Card.Img variant="top" />
         <Card.Body>
           <Card.Title>Card Title</Card.Title>
           <Card.Text>
             Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+            bulk of the cards content.
           </Card.Text>
           <Button variant="primary">Go somewhere</Button>
         </Card.Body>
       </Card>
-    </>
+    </div>
   );
 };
