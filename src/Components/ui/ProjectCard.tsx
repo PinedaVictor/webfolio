4b3a4d652ce8c368285a80c6a761436364957e2c
unknown <pinedavictor095@gmail.com>
2020-12-23 01:49:23 -0800

    Added transition to Project card

--- src/Components/ui/ProjectCard.tsx
+++ src/Components/ui/ProjectCard.tsx
@@ -1,7 +1,25 @@
-import React from "react";
-import { Col } from "react-bootstrap";
+import React, { useState } from "react";
+import { Col, Button } from "react-bootstrap";
+import { useTransition, animated } from "react-spring";
 
 export const ProjectCard: React.FC = () => {
+  const [info, toggle] = useState(false);
+  const transition = useTransition(info, null, {
+    config: {
+      duration: 425,
+    },
+    from: {
+      transform: "translate3d(0, 100%, 0)",
+      opacity: 1,
+      width: "100%",
+      backgroundColor: "red",
+      height: "30rem",
+      float: "right",
+      position: "absolute",
+    },
+    enter: { transform: "translate3d(0, -10%, 0)" },
+    leave: { transform: "translate3d(0, 10%, 0)" },
+  });
   return (
     <Col
       xs={12}
@@ -11,7 +29,18 @@ export const ProjectCard: React.FC = () => {
         backgroundColor: "grey",
         height: "30rem",
         left: "0",
+        border: "1px solid black",
       }}
-    ></Col>
+    >
+      <Button onClick={() => toggle(!info)}></Button>
+      {transition.map(
+        ({ item, key, props }) =>
+          item && (
+            <Col sm={12}>
+              <animated.div key={key} style={props}></animated.div>
+            </Col>
+          )
+      )}
+    </Col>
   );
 };
