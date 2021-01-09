0a8d911545db47a1c5398e32962a801702bfd678
unknown <pinedavictor095@gmail.com>
2021-01-09 13:23:53 -0800

    Added Ai and Ps icons

--- src/Assets/icons/index.tsx
+++ src/Assets/icons/index.tsx
@@ -11,3 +11,5 @@ export { ReactComponent as LeftBrace } from "./leftBrace.svg";
 export { ReactComponent as RightBrace } from "./rightBrace.svg";
 export { ReactComponent as LB } from "./LB.svg";
 export { ReactComponent as Icons } from "./Icons.svg";
+export { ReactComponent as Ai } from "./ai.svg";
+export { ReactComponent as Ps } from "./ps.svg";
