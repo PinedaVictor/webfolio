7727e8a355315f05f8271f64c144574d4857f6e8
PinedaVictor <pinedavictor095@gmail.com>
2021-01-21 15:58:29 -0800

    Commented out google extended eslint rules

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
     "plugin:import/errors",
     "plugin:import/warnings",
     "plugin:import/typescript",
-    "google",
+    // "google",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
@@ -19,10 +19,7 @@ module.exports = {
   ignorePatterns: [
     "/lib/**/*", // Ignore built files.
   ],
-  plugins: [
-    "@typescript-eslint",
-    "import",
-  ],
+  plugins: ["@typescript-eslint", "import"],
   rules: {
     quotes: ["error", "double"],
   },
