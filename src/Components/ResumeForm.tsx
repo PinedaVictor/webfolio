86d1c33c70a12bf3ff8878e7e7095d60a7fed66b
PinedaVictor <pinedavictor095@gmail.com>
2021-01-25 12:18:09 -0800

    Added input handling to resume form

--- src/Components/ResumeForm.tsx
+++ src/Components/ResumeForm.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Col, Button } from "react-bootstrap";
 import { IoMdClose } from "react-icons/io";
 
-interface ResumeForm {
+interface ResumeFormFields {
   name: string;
   email: string;
 }
@@ -11,6 +11,27 @@ interface ResumeFormProps {
   toggleForm: () => void;
 }
 export const ResumeForm: React.FC<ResumeFormProps> = (props) => {
+  const [formFields, setFormFields] = useState<ResumeFormFields>({
+    name: "",
+    email: "",
+  });
+
+  const submitResmueForm = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("Clicking submit resume form with::", formFields);
+  };
+  //   FIXME: React.ChangeEvent<FormControlElement>
+  const handleInputChange = (event: any) => {
+    event.preventDefault();
+    const name = event.target.name;
+    const value = event.target.value;
+    setFormFields((prevState) => {
+      const data = Object.assign({}, prevState);
+      const newData = Object.assign(data, { ...data, [name]: value });
+      return newData;
+    });
+    console.log;
+  };
   return (
     <Col
       xs={12}
@@ -47,6 +68,7 @@ export const ResumeForm: React.FC<ResumeFormProps> = (props) => {
         {"Enter your email and my resume will be sent"}
       </p>
       <Form
+        onSubmit={submitResmueForm}
         style={{
           maxWidth: "35rem",
           margin: "auto",
@@ -54,7 +76,13 @@ export const ResumeForm: React.FC<ResumeFormProps> = (props) => {
         }}
       >
         <Form.Group controlId="name">
-          <Form.Control type="name" placeholder="Name" />
+          <Form.Control
+            type="name"
+            name="name"
+            placeholder="Name"
+            onChange={(e) => handleInputChange(e)}
+            value={formFields.name}
+          />
         </Form.Group>
         <Form.Group controlId="formBasicEmail">
           <Form.Text
@@ -67,7 +95,13 @@ export const ResumeForm: React.FC<ResumeFormProps> = (props) => {
           >
             {"Your email will NOT be shared with anyone else."}
           </Form.Text>
-          <Form.Control type="email" placeholder="Email" />
+          <Form.Control
+            type="email"
+            name="email"
+            placeholder="Email"
+            onChange={(e) => handleInputChange(e)}
+            value={formFields.email}
+          />
         </Form.Group>
 
         <Button
