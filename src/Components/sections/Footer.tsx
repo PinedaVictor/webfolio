9d8176f454b4c456bb9a5794abff7ef922814dfb
PinedaVictor <pinedavictor095@gmail.com>
2021-01-27 00:07:06 -0800

    Added footer scss

--- src/Components/sections/Footer.tsx
+++ src/Components/sections/Footer.tsx
@@ -1,40 +1,14 @@
 import React from "react";
 import "../../Styles/main.scss";
-import { Row, Col, Container } from "react-bootstrap";
+import { Row, Container } from "react-bootstrap";
 
 export const Footer: React.FC = () => {
   return (
-    <Container
-      fluid
-      style={{
-        backgroundColor: "",
-      }}
-    >
+    <Container fluid>
       <Row style={{ textAlign: "center" }}>
-        <div
-          className="contactButton"
-          style={{ height: "2px", width: "100%", marginBottom: "1rem" }}
-        />
-        <p
-          style={{
-            textAlign: "center",
-            width: "100%",
-            color: "white",
-            fontSize: "2rem",
-            textDecoration: "underline",
-          }}
-        >
-          All Projects
-        </p>
-        <p
-          style={{
-            textAlign: "center",
-            width: "100%",
-            color: "white",
-          }}
-        >
-          &#169; Victor Pineda All Rights Reserved, 2021
-        </p>
+        <div className="footerDivider" />
+        <p className="footerLink">All Projects</p>
+        <p id="copyRight">&#169; Victor A. Pineda All Rights Reserved, 2021</p>
       </Row>
     </Container>
   );
