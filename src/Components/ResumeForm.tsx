f79bf37037bb3a9f57106aa9159dbff2169b6ec4
unknown <pinedavictor095@gmail.com>
2021-02-01 06:20:33 -0800

    Implemented sending status and error handling for resume form

--- src/Components/ResumeForm.tsx
+++ src/Components/ResumeForm.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import "../Styles/components/contactForm.scss";
-import { Form, Col, Button } from "react-bootstrap";
-import { IoMdClose } from "react-icons/io";
+import { Form, Col, Button, Spinner } from "react-bootstrap";
+import { animated, useTransition } from "react-spring";
+import { IoMdClose, IoMdCheckmark } from "react-icons/io";
 import firebase from "firebase/app";
 import "firebase/functions";
 
@@ -18,17 +19,28 @@ export const ResumeForm: React.FC<ResumeFormProps> = (props) => {
     name: "",
     email: "",
   });
+  const [sendingEmail, setSendingEmail] = useState(false);
+  const [emailSent, setEmailSent] = useState(false);
+  const [emailFailed, setEmailFailed] = useState(false);
 
-  const submitResmueForm = (event: React.FormEvent<HTMLFormElement>) => {
+  const submitResumeForm = (event: React.FormEvent<HTMLFormElement>) => {
+    setSendingEmail(true);
     event.preventDefault();
     console.log("Clicking submit resume form with::", formFields);
     const sendEmail = firebase.functions().httpsCallable("sendResume");
     sendEmail(formFields)
       .then(() => {
         setFormFields({ name: "", email: "" });
+        setEmailSent(true);
+        setTimeout(() => {
+          setSendingEmail(false);
+        }, 1000);
       })
-      .catch((error) => {
-        console.log("Error sending resume:::", error);
+      .catch(() => {
+        setEmailFailed(true);
+        setTimeout(() => {
+          setSendingEmail(false);
+        }, 1700);
       });
   };
   //   FIXME: React.ChangeEvent<FormControlElement>
@@ -43,6 +55,51 @@ export const ResumeForm: React.FC<ResumeFormProps> = (props) => {
     });
     console.log;
   };
+
+  const emailSendingStatusTransition = useTransition(sendingEmail, null, {
+    config: {
+      duration: 450,
+    },
+    from: {
+      opacity: 0,
+      width: "100%",
+      backgroundColor: "rgba(0,0,0,0.9)",
+      height: "30rem",
+      position: "absolute",
+      left: "0",
+      top: "-25rem",
+      overflow: "hidden",
+      borderRadius: "25px",
+      boxShadow: "10px 10px 20px black, -10px 10px 20px black",
+    },
+    enter: {
+      opacity: 50,
+    },
+    leave: { opacity: 0 },
+  });
+
+  const emailSentTransition = useTransition(emailSent, null, {
+    config: {
+      duration: 450,
+    },
+    from: {
+      opacity: 0,
+      width: "100%",
+      backgroundColor: "rgba(0,0,0,0.9)",
+      height: "30rem",
+      position: "absolute",
+      left: "0",
+      top: "-25rem",
+      overflow: "hidden",
+      borderRadius: "25px",
+      boxShadow: "10px 10px 20px black, -10px 10px 20px black",
+    },
+    enter: {
+      opacity: 50,
+    },
+    leave: { opacity: 0 },
+  });
+
   return (
     <Col className="colWrapper" xs={12} sm={12} md={12} lg={12} xl={12}>
       <div>
@@ -54,7 +111,7 @@ export const ResumeForm: React.FC<ResumeFormProps> = (props) => {
         {"Enter your email and my resume will be sent"}
       </p>
       <Form
-        onSubmit={submitResmueForm}
+        onSubmit={submitResumeForm}
         style={{
           maxWidth: "35rem",
           margin: "auto",
@@ -87,6 +144,97 @@ export const ResumeForm: React.FC<ResumeFormProps> = (props) => {
           Submit
         </Button>
       </Form>
+      {emailSendingStatusTransition.map(
+        ({ item, key, props }) =>
+          item && (
+            <Col key={key} sm={12}>
+              <animated.div key={key} style={props}>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    flexWrap: "wrap",
+                    justifyContent: "center",
+                    marginTop: "10rem",
+                  }}
+                >
+                  {!emailFailed ? (
+                    <p
+                      style={{
+                        fontSize: "4rem",
+                        width: "100%",
+                        textAlign: "center",
+                        fontFamily: "San Francisco Text",
+                        color: "white",
+                      }}
+                    >
+                      Sending
+                    </p>
+                  ) : (
+                    <p
+                      style={{
+                        fontSize: "3rem",
+                        width: "100%",
+                        textAlign: "center",
+                        fontFamily: "San Francisco Text",
+                        color: "white",
+                      }}
+                    >
+                      Error sending email
+                    </p>
+                  )}
+                  <Spinner
+                    animation="grow"
+                    style={{
+                      width: "5rem",
+                      height: "5rem",
+                      color: !emailFailed ? "#00c5d4" : "red",
+                    }}
+                  />
+                </div>
+              </animated.div>
+            </Col>
+          )
+      )}
+      {emailSentTransition.map(
+        ({ item, key, props }) =>
+          item && (
+            <Col key={key} sm={12}>
+              <animated.div key={key} style={props}>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    flexWrap: "wrap",
+                    justifyContent: "center",
+                    marginTop: "10rem",
+                  }}
+                >
+                  <p
+                    style={{
+                      fontSize: "3rem",
+                      width: "100%",
+                      textAlign: "center",
+                      fontFamily: "San Francisco Text",
+                      color: "white",
+                    }}
+                  >
+                    Email Sent
+                  </p>
+                  <IoMdCheckmark
+                    size="5rem"
+                    color="white"
+                    style={{
+                      borderRadius: "50%",
+                      backgroundColor: "#00c5d4",
+                      padding: "7px",
+                    }}
+                  />
+                </div>
+              </animated.div>
+            </Col>
+          )
+      )}
     </Col>
   );
 };
